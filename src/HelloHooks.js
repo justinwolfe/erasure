1079ff9666f8cd3e03a8edba35334cb150939ff1
Glitch (starter-react-template) <none>
2019-06-16 19:32:09 +0000

    🚖🍓 Checkpoint ./src/HelloHooks.js:4745711/1873

--- src/HelloHooks.js
+++ src/HelloHooks.js
@@ -1,20 +1,37 @@
-import React, { useState } from 'react'
+import React, { useState , useEffect } from 'react'
 
 function HelloHooks() {
   
-    const [posts , setPosts] = useState(() => {
+    const [posts , setPosts] = useState( () => {
        let posts = [] 
-      fetch('https://jsonplaceholder.typicode.com/todos/1')
-      .then(response => response.json())
-      .then(json => { posts = json })
+            
+      async function test() {
+        fetch('https://jsonplaceholder.typicode.com/todos')
+        .then(response => response.json())
+        .then(json => { console.log('json',json); posts = json })
+
+        console.log('posts in usestate', posts)
+        return posts; 
+        
+      }
+      
+      return test()
     })
   
+    const [post , setPost] = useState({userId: 1, id: 1, title: "delectus aut autem", completed: false})
+    
+    useEffect(() => {
+      console.log('posts',posts)
+              setPosts(posts); 
+     })
   
     return (
       <>
           <p>Hello Hooks</p>
         <>
           { JSON.stringify(posts) }
+          <hr />
+          { JSON.stringify(post) }
           </>
       </>
     )
