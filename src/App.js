9838c8f30ed40973e02c98a91394676960f74a6c
Glitch (starter-react-template) <none>
2019-06-16 19:54:06 +0000

    👠🔖 Checkpoint ./src/HelloHooks.js:4745711/152 ./src/HelloHooks2.js:4745711/1445 ./src/App.js:4745711/64

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import HelloHooks from './HelloHooks'
+import HelloHooks2 from './HelloHooks2'
 class App extends Component {
   render() {
     return (
       <div className="App">
         <HelloHooks />
+        <HelloHooks2 />
       </div>
     );
   }
--- src/HelloHooks2.js
+++ src/HelloHooks2.js
@@ -1,6 +1,6 @@
-import React, { useState , useEffect } from 'react'
+import React, { useState , useEffect, useReducer, useCallback } from 'react'
 
-function HelloHooks() {
+function HelloHooks2() {
   
 //     const [posts , setPosts] = useState( () => {
 //        // let posts = [] 
@@ -23,18 +23,18 @@ function HelloHooks() {
   const [post , setPost] = useState({userId: 1, id: 1, title: "delectus aut autem", completed: false})
     
     useEffect(() => {
-      console.log('posts',posts)
-      if(!posts.userId){
-        fetch('https://jsonplaceholder.typicode.com/todos/1')
-        .then(response => response.json())
-        .then(json => { console.log('json',json); setPosts(json);  })
-              // setPosts(posts); 
-        }
+      // console.log('posts',posts)
+      // if(!posts.length){
+      //   fetch('https://jsonplaceholder.typicode.com/todos/1')
+      //   .then(response => response.json())
+      //   .then(json => { console.log('json',json); setPosts(json);  })
+      //         // setPosts(posts); 
+      //   }
      }, [posts])
   
     return (
       <>
-          <p>Hello Hooks</p>
+          <p>Hello Hooks2</p>
         <>
           { JSON.stringify(posts) }
           <hr />
@@ -44,4 +44,4 @@ function HelloHooks() {
     )
 }
 
-export default HelloHooks
+export default HelloHooks2
