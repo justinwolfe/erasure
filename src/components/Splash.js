74a0e0dd9665ac2a2d2c347c348f347b9cce2a2e
Glitch (starter-react-template) <none>
2019-11-17 18:02:26 +0000

    🍇🐇 Checkpoint ./src/components/Splash.js:3258259/549

--- src/components/Splash.js
+++ src/components/Splash.js
@@ -1,6 +1,9 @@
 import React, {useState, useEffect} from "react";
 import Mercury from '@postlight/mercury-parser';
 
+//using Google as a proxy to avoid paywall issues
+const addProxy = url => `http://googleweblight.com/?lite_url=${url}`
+
 const Splash = () => {
   const [url, setUrl] = useState('')
   const [fetchStatus, setFetchStatus] = useState({})
@@ -8,14 +11,24 @@ const Splash = () => {
   
   useEffect(() => {
     if(fetchStatus === 'fetching'){
-      Mercury.parse(url).then(data => {
+      fetch(url)
+        .then(res => res.html())
+        .then(data => {
+          console.log(data)
+          setPage(data)
+        })
+        .catch(err => {
+        setPage(err)
+      })
+      setFetchStatus('fetched')
+      }
+      /*Mercury.parse(url).then(data => {
         setPage(data)
         setFetchStatus('fetched')
       }).catch(err => {
         setPage(err)
         setFetchStatus('fetched')
-      })
-    }
+      })*/
   }, [fetchStatus,url])
   
   return <div>
