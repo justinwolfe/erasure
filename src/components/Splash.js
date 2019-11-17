784ffc017977acd7f84f51ee30d57c7be51107c5
Glitch (starter-react-template) <none>
2019-11-17 18:14:26 +0000

    🐝⛽️ Checkpoint ./src/components/Splash.js:3258259/2639

--- src/components/Splash.js
+++ src/components/Splash.js
@@ -1,45 +1,49 @@
-import React, {useState, useEffect} from "react";
-import Mercury from '@postlight/mercury-parser';
+import React, { useState, useEffect } from "react";
+import Mercury from "@postlight/mercury-parser";
 
 //using Google as a proxy to avoid paywall issues
-const addProxy = url => `http://googleweblight.com/?lite_url=${url}`
+const addProxy = url => `http://googleweblight.com/?lite_url=${url}`;
 
 const Splash = () => {
-  const [url, setUrl] = useState('')
-  const [fetchStatus, setFetchStatus] = useState({})
-  const [page, setPage] = useState({})
-  
+  const [url, setUrl] = useState("");
+  const [fetchStatus, setFetchStatus] = useState({});
+  const [page, setPage] = useState('');
+
   useEffect(() => {
-    if(fetchStatus === 'fetching'){
+    if (fetchStatus === "fetching") {
       fetch(url)
         .then(res => res.html())
         .then(data => {
-          console.log(data)
-          setPage(data)
+          console.log(data);
+          setPage(data);
+          setFetchStatus('fetched')
         })
         .catch(err => {
-        setPage(err)
-      })
-      setFetchStatus('fetched')
-      }
-      /*Mercury.parse(url).then(data => {
+          alert(err)
+          setPage('butt');
+          setFetchStatus('fetched')
+        });
+    }
+    /*Mercury.parse(url).then(data => {
         setPage(data)
         setFetchStatus('fetched')
       }).catch(err => {
         setPage(err)
         setFetchStatus('fetched')
       })*/
-  }, [fetchStatus,url])
-  
-  return <div>
-    <h1>erase-mark</h1>
+  }, [fetchStatus, url]);
+
+  return (
     <div>
-      <p>Enter a URL</p>
-      <input value={url} onChange={(e) => setUrl(e.target.value)}/>
+      <h1>erase-mark</h1>
+      <div>
+        <p>Enter a URL</p>
+        <input value={url} onChange={e => setUrl(e.target.value)} />
+      </div>
+      <button onClick={() => setFetchStatus("fetching")}>Start</button>
+      <div>{page}</div>
     </div>
-    <button onClick={() => setFetchStatus('fetching')}>Start</button>
-    <div>{JSON.stringify(page)}</div>
-  </div>;
+  );
 };
 
 export default Splash;
