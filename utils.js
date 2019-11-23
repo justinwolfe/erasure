2d6ca0c174b174db9eb1d9330ff4f458803cfd5d
Glitch (starter-react-template) <none>
2019-11-23 18:16:36 +0000

    🛋💳 Checkpoint ./utils.js:3258259/1275

---
+++ utils.js
@@ -0,0 +1,26 @@
+import React, { useState, useEffect } from "react";
+import Mercury from "@postlight/mercury-parser";
+import remove from "remove-markdown";
+
+const addProxy = url => `https://cors-anywhere.herokuapp.com/${url}`;
+
+const getContentFromUrl = url => new Promise((resolve, reject) => {
+  fetch(addProxy(url))
+        .then(res => res.text())
+        .then(htmlString => {
+          Mercury.parse(url, {
+            html: htmlString,
+            contentType: "markdown"
+          }).then(result => {
+            if (result.content) {
+              const cleaned = remove(result.content);
+              resolve(cleaned)
+            } else {
+              resolve(JSON.stringify(result));
+            }
+          });
+        })
+        .catch(err => {
+          reject(JSON.stringify(err))
+        }); 
+});
