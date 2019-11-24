aad7fec632405d1c43f66c71538016b9f69d2475
Glitch (starter-react-template) <none>
2019-11-24 00:43:44 +0000

    👳🚞 Checkpoint ./src/components/Splash.js:3258259/18 ./src/utils.js:3258259/4703

--- src/utils.js
+++ src/utils.js
@@ -4,23 +4,30 @@ import remove from "remove-markdown";
 
 const addProxy = url => `https://cors-anywhere.herokuapp.com/${url}`;
 
-export const getContentFromUrl = url => new Promise((resolve, reject) => {
-  fetch(addProxy(url))
-        .then(res => res.text())
-        .then(htmlString => {
-          Mercury.parse(url, {
-            html: htmlString,
-            contentType: "markdown"
-          }).then(result => {
-            if (result.content) {
-              const cleaned = remove(result.content);
-              resolve(cleaned)
-            } else {
-              reject(JSON.stringify(result));
-            }
-          });
-        })
-        .catch(err => {
-          reject(JSON.stringify(err))
-        }); 
-});
+export const getContentFromUrl = url =>
+  new Promise((resolve, reject) => {
+    fetch(addProxy(url))
+      .then(res => res.text())
+      .then(htmlString => {
+        Mercury.parse(url, {
+          html: htmlString,
+          contentType: "markdown"
+        }).then(result => {
+          if (result.content) {
+            const cleaned = remove(result.content)
+              .replace(/[\[\]\(\)]/gm, "")
+              .replace(
+                /^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/gm,
+                ""
+              )
+              .replace(/\n\s*\n/g, "\n\n");
+            resolve(cleaned);
+          } else {
+            reject(JSON.stringify(result));
+          }
+        });
+      })
+      .catch(err => {
+        reject(JSON.stringify(err));
+      });
+  });
