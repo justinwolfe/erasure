577898473abae5fdb30d556f82f31b9a4e3ad0b7
Glitch (starter-react-template) <none>
2019-11-29 23:48:17 +0000

    🌃🏍 Checkpoint ./src/App.js:3258259/6737 ./src/components/Word.js:3258259/1239

--- src/App.js
+++ src/App.js
@@ -7,21 +7,25 @@ import "./App.css";
 const App = () => {
   const [content, setContent] = useState(undefined);
   const [currentError, setCurrentError] = useState("");
-  
-  const toggleElement = (id) => {
-    const [paragraphIndex, wordIndex, characterIndex] = id.split("-")
-    const newContent = {...content}
+
+  const toggleElement = id => {
+    const [paragraphIndex, wordIndex, characterIndex] = id.split("-");
+    const newContent = { ...content };
     const word = newContent.paragraphs[paragraphIndex].words[wordIndex];
-    word.isVisible = !word.isVisible
-    setContent(newContent)
-  }
-  
+    word.isVisible = !word.isVisible;
+    console.log("before set", newContent.paragraphs[0].words[0])
+    setContent(newContent);
+  };
+
+  console.log("render", content && content.paragraphs && content.paragraphs[0].words[0])
   return (
-    <div style={{margin:'10%'}}className="App">
+    <div style={{ margin: "10%" }} className="App">
       {(!content && (
         <Splash setContent={setContent} setCurrentError={setCurrentError} />
       )) ||
-        (content && <Editor content={content} toggleElement={toggleElement} />) || <div>default</div>}
+        (content && (
+          <Editor content={content} toggleElement={toggleElement} />
+        )) || <div>default</div>}
     </div>
   );
 };
--- src/components/Word.js
+++ src/components/Word.js
@@ -1,12 +1,26 @@
 import React, { useState, useEffect } from "react";
 
+const styles = {
+  visible: {
+    display: "inline-block"
+  },
+  invisible: {
+    display: "hidden"
+  }
+};
+
 const Word = ({ characters, id, isVisible, toggleElement }) => {
   const word = characters.map(character => character.character).join("");
   return (
     <React.Fragment>
-      <span className={isVisible} onClick={(e) => {
-          toggleElement(id)
-        }}>{word}</span>{" "}
+      <span
+        style={isVisible ? styles.visible : styles.invisible}
+        onClick={e => {
+          toggleElement(id);
+        }}
+      >
+        {word}{isVisible}
+      </span>{" "}
     </React.Fragment>
   );
 };
