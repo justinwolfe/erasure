0cd098f37a50142bb9448fd2dbcb64924d21b597
Glitch (starter-react-template) <none>
2019-11-29 19:13:13 +0000

    🦁😬 Checkpoint ./src/components/Editor.js:3258259/27 ./src/App.js:3258259/998

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
-import React from "react";
+import React, {useState} from "react";
 import Splash from "./components/Splash.js";
+import Editor from "./components/Editor.js"
 
 import "./App.css";
 
+const RegisteredComponents = {
+  splash: <Splash/>,
+  editor: <Editor/>,
+}
+
 const App = () => {
+  const [content, setContent] = useState('')
+  const [currentScreen, setCurrentScreen] = useState('Splash')
+  
   return (
     <div className="App">
+      {
+        currentScreen === "Splash" && !content (
+          <Splash setContent={setContent} />
+        )
+        || currentScreen === "Editor" && (
+          <Editor/>
+        )
+        || <div>default</div>
+      }
       <Splash />
     </div>
   );
--- src/components/Editor.js
+++ src/components/Editor.js
@@ -3,3 +3,5 @@ import React, { useState, useEffect } from "react";
 const Editor = ({}) => {
   
 }
+
+export default Editor
