bd5d04e42a6f1991c5a32c7ad78a293968344663
Glitch (erasure) <none>
2019-12-01 23:29:19 +0000

    ☂️🎐 Checkpoint ./src/components/Editor.js:3258259/11640

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -1,44 +1,53 @@
 import React, { useState, useEffect } from "react";
 import Word from "./Word";
 
+const style = {
+  textAlign: "left",
+  wordWrap: "break-word",
+  cursor: "pointer",
+  userSelect: "none"
+};
+
 const Editor = ({ content, toggleElement }) => {
   const { paragraphs, url, created } = content;
-  const [currentTouchState, setCurrentTouchState] = useState(undefined);
+  const [currentTouchState, setCurrentTouchState] = useState(true);
 
   const handleTouchStart = e => {
+    console.log("ts", currentTouchState);
     const firstTouch = e.target.getAttribute("data-visible");
-    setCurrentTouchState();
+    if (firstTouch) {
+      setCurrentTouchState(firstTouch);
+    }
     const key = e.target.getAttribute("name");
     if (key) {
       toggleElement(key);
     }
   };
 
+  const handleTouchEnd = e => {
+    console.log("te");
+    setCurrentTouchState(undefined);
+  };
+
   const handleTouchMove = e => {
+    console.log("tm");
     const { clientX, clientY } = e.changedTouches[0];
     const movedIntoElement = document.elementFromPoint(clientX, clientY);
     if (movedIntoElement) {
       const key = movedIntoElement.getAttribute("name");
       const visible = movedIntoElement.getAttribute("data-visible");
       if (key) {
-        toggleElement(key, false);
+        toggleElement(key, currentTouchState);
       }
     }
   };
 
   return (
     <div
-      style={{
-        textAlign: "left",
-        wordWrap: "break-word",
-        cursor: "pointer",
-        userSelect: "none"
-      }}
-      onTouchStart={e => handleTouchStart}
-      onTouchMove={e => handleTouchMove}
-      onTouchEnd={e => {
-        console.log("te");
-      }}
+      style={style}
+      onTouchStart={handleTouchStart}
+      onTouchMove={handleTouchMove}
+      onTouchEnd={handleTouchEnd}
     >
       {paragraphs &&
         paragraphs.map(paragraph => (
