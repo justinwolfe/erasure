f18f6fe0b58cde3e80425cbc3263b14a89873ea2
Glitch (erasure) <none>
2020-01-28 21:33:11 +0000

    🛥🕋 Checkpoint ./src/App.js:3258259/110 ./src/components/Controls.js:3258259/46

--- src/App.js
+++ src/App.js
@@ -4,14 +4,9 @@ import Editor from "./components/Editor.js";
 
 import "./App.css";
 
-const initialStyle = {
-  margin: "10%",
-};
-
 const App = () => {
   const [content, setContent] = useState(undefined);
   const [currentError, setCurrentError] = useState("");
-  const [style, setStyle] = useState(initialStyle);
 
   const toggleElement = (id, value) => {
     const [paragraphIndex, wordIndex, characterIndex] = id.split("-");
@@ -28,7 +23,7 @@ const App = () => {
   };
 
   return (
-    <div style={style} className="App">
+    <div className="App">
       <Splash setContent={setContent} setCurrentError={setCurrentError} />
       {content && <Editor content={content} toggleElement={toggleElement} />}
     </div>
