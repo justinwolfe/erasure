e0555d41557c3895bc877e3ae62dbf29ca41c4fd
Glitch (erasure) <none>
2020-10-18 20:37:50 +0000

    🏔🎹 Checkpoint ./src/App.js:3258259/4181

--- src/App.js
+++ src/App.js
@@ -12,33 +12,36 @@ const App = () => {
     if (!id) {
       return undefined;
     }
-    const [paragraphIndex, wordIndex, characterIndex] = id.split("-");
+    const [paragraphIndex, wordIndex] = id.split("-");
     const word = content.paragraphs[paragraphIndex].words[wordIndex];
     return word;
   };
 
   const editWord = id => {
     const word = getWord(id);
-    console.log(word)
     alert(word.characters.join(""));
   };
 
   const toggleMark = (id, value) => {
-    if (!id) {
-      return undefined;
-    }
-    const [paragraphIndex, wordIndex, characterIndex] = id.split("-");
-    const newContent = { ...content };
-    const word = newContent.paragraphs[paragraphIndex].words[wordIndex];
+    const word = getWord(id);
+
     if (!word) {
       return undefined;
     }
+
     if (value === false || value === true) {
       word.isMarked = value;
     } else {
       word.isMarked = !word.isMarked;
     }
-    setContent(newContent);
+
+    const [paragraphIndex, wordIndex] = id.split("-");
+
+    const updatedContent = { ...content };
+
+    updatedContent.paragraphs[paragraphIndex].words[wordIndex] = word;
+
+    setContent(updatedContent);
     return word.isMarked;
   };
 
