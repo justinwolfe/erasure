1f8166af87c3241949ca20f921f7b6e2e8c3e4c1
Glitch (erasure) <none>
2020-11-07 00:13:19 +0000

    👭🚜 Checkpoint ./src/components/CharacterEditor.js:3258259/86 ./src/components/WordEditor.js:3258259/1326

--- src/components/CharacterEditor.js
+++ src/components/CharacterEditor.js
@@ -1,7 +1,10 @@
 import React, { useState, useEffect, useRef } from "react";
 
 const style ={
-  display:"inlineBlock"
+  display:"inline-block",
+  border:"1px solid black",
+  padding:"2%",
+  margin:"2%"
 }
 
 const CharacterEditor = ({ character }) => <div style={style}>{character}</div>;
--- src/components/WordEditor.js
+++ src/components/WordEditor.js
@@ -1,13 +1,24 @@
 import React, { useState, useEffect, useRef } from "react";
-import Modal from './Modal'
-import CharacterEditor from "./CharacterEditor"
+import Modal from "./Modal";
+import CharacterEditor from "./CharacterEditor";
 
-const WordEditor = ({word, close, editWord}) => {
- return <Modal close={close}>
-   <div>
-     {word.characters.map(({character, id}) => <CharacterEditor key={id} character={character} />)}
-   </div>
- </Modal>
-}
+const WordEditor = ({ word, close, editWord }) => {
+  const [characters, setCharacters] = useState(word.characters);
+  console.log(word.characters);
+  
+  const toggleMark = (id) => {
+    
+  }
+  
+  return (
+    <Modal close={close}>
+      <div>
+        {word.characters.map(({ character, id, isMarked }) => (
+          <CharacterEditor key={id} character={character} isMarked={isMarked} const toggleMark={toggleMark} />
+        ))}
+      </div>
+    </Modal>
+  );
+};
 
-export default WordEditor
+export default WordEditor;
