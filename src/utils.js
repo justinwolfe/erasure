fc3ef70272db343e1474284282e5211a93963137
Glitch (erasure) <none>
2021-01-17 00:33:17 +0000

    😽🏅 Checkpoint ./src/utils.js:3258259/121

--- src/utils.js
+++ src/utils.js
@@ -5,6 +5,10 @@ import { saveAs } from "file-saver";
 
 const addProxy = url => `https://api.codetabs.com/v1/proxy?quest=${url}`;
 
+const convertToStateV2 = document => {
+  
+}
+
 const convertToState = document =>
   document.split("\n\n").map((paragraph, paragraphIndex) => ({
     words: paragraph.split(" ").map((word, wordIndex) => ({
